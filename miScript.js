1aab043d7ae59c1ae6e8364846578b67defcdcdf
monicahuidobro <monicahuidobro@hotmail.com>
2022-12-30 11:37:16 +0100

    VERSION FINAL

--- miScript.js
+++ miScript.js
@@ -1,14 +1,21 @@
 //Taking the inputs from the form
-const dice1 = document.getElementById("dice1");
-const dice2 = document.getElementById("dice2");
-const dice3 = document.getElementById("dice3");
-const dice4 = document.getElementById("dice4");
 
-function collectingInput(event) {
+function collectingInput1 (event) {
   event.preventDefault();
-  const diceNumberInput = document.getElementById('diceNumber');
+  calculation("diceNumber1", "diceType1", "randomNumber1","dice1-1", "dice2-1", "dice3-1", "dice4-1");
+  document.getElementById("button1").disabled = true;
+}
+function collectingInput2 (event) {
+  event.preventDefault();
+  calculation("diceNumber2", "diceType2", "randomNumber2","dice1-2", "dice2-2", "dice3-2", "dice4-2");
+  document.getElementById("button2").disabled = true;
+}
+
+
+function calculation(number, type , random, dice1, dice2, dice3, dice4) {
+  const diceNumberInput = document.getElementById(number);
   const diceNumber = diceNumberInput.value;
-  const diceTypeInputs = document.getElementsByName('diceType');
+  const diceTypeInputs = document.getElementsByName(type);
   let diceType;
   for (let i = 0; i < diceTypeInputs.length; i++) {
     if (diceTypeInputs[i].checked) {
@@ -23,42 +30,53 @@ function collectingInput(event) {
     arrDice[i] = Math.floor(Math.random()*diceType)+1;
     sum = sum + arrDice[i]; 
   };
-  const randomNumber = document.getElementById("randomNumber");
-  dice1.textContent=`${arrDice[0]}`;
-  dice2.textContent=`${arrDice[1]}`;
-  dice3.textContent=`${arrDice[2]}`;
-  dice4.textContent=`${arrDice[3]}`;
+  const randomNumber = document.getElementById(random);
+  document.getElementById(dice1).textContent=`${arrDice[0]}`;
+  document.getElementById(dice2).textContent=`${arrDice[1]}`;
+  document.getElementById(dice3).textContent=`${arrDice[2]}`;
+  document.getElementById(dice4).textContent=`${arrDice[3]}`;
   randomNumber.textContent = `${sum} `;
 
   switch (diceNumber){
-    case "1":
-      dice1.style.display= "flex";
-      dice2.style.display= "none";
-      dice3.style.display= "none";
-      dice4.style.display= "none";
-      break;
     case "2":
-      dice1.style.display= "flex";
-      dice2.style.display= "flex";
-      dice3.style.display= "none";
-      dice4.style.display= "none";
+      document.getElementById(dice2).style.display= "flex";
       break;
     case "3":
-      dice1.style.display= "flex";
-      dice2.style.display= "flex";
-      dice3.style.display= "flex";
-      dice4.style.display= "none";
+      document.getElementById(dice2).style.display= "flex";
+      document.getElementById(dice3).style.display= "flex";
       break;
     case "4":
-      dice1.style.display= "flex";
-      dice2.style.display= "flex";
-      dice3.style.display= "flex";
-      dice4.style.display= "flex";
+      document.getElementById(dice2).style.display= "flex";
+      document.getElementById(dice3).style.display= "flex";
+      document.getElementById(dice4).style.display= "flex";
       break;
-          }
-}
+  }
+};
+
+document.getElementById("restart").disabled = true;
 
 
 
 
+document.getElementById("solve").addEventListener("click", function() {
+  if(document.getElementById("randomNumber1").textContent > document.getElementById("randomNumber2").textContent) {
+    document.getElementById("winner").textContent = "Player 1 wins";
+    document.getElementById("player1").style.backgroundColor="rgb(162, 255, 139)";
+    document.getElementById("solve").disabled = true;
+    document.getElementById("restart").disabled = false;
+  } else if (document.getElementById("randomNumber1").textContent < document.getElementById("randomNumber2").textContent) {
+    document.getElementById("winner").textContent = "Player 2 wins";
+    document.getElementById("player2").style.backgroundColor="rgb(162, 255, 139)";
+    document.getElementById("solve").disabled = true;
+    document.getElementById("restart").disabled = false;
+  } else if (document.getElementById("randomNumber1").textContent === document.getElementById("randomNumber2").textContent){
+    document.getElementById("winner").textContent = "Tie";
+    document.getElementById("solve").disabled = true;
+    document.getElementById("restart").disabled = false;
+  }
+});
+
+document.getElementById("restart").addEventListener("click", function() {
+  window.location.reload(true);
+});
 
